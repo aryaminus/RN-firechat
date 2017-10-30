e1680587753e39be632d33267b7193e9eb257ff8
Sunim Acharya <sunim.54@gmail.com>
2017-10-30 11:57:59 +0545

    Added navigator and files from signin/up screen

--- App.js
+++ App.js
@@ -9,49 +9,77 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  StatusBar
 } from 'react-native';
 
-const instructions = Platform.select({
+import Login from "./app/components/Login";
+import Boiler from "./app/components/Boiler";
+import ForgetPassword from "./app/components/ForgetPassword";
+import Register from "./app/components/Register";
+
+import { StackNavigator } from "react-navigation";
+
+/*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+});*/
 
-export default class App extends Component<{}> {
+class Home extends Component<{}> {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#16a085",
+      elevation: null
+    },
+    header: null
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <StatusBar barStyle="light-content" backgroundColor="#16a085" />
+        <Login navigation={this.props.navigation} />
       </View>
     );
   }
 }
 
+export default App = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "Home"
+    }
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      title: "Login"
+    }
+  },
+  Register: {
+    screen: Register,
+    navigationOptions: {
+      title: "Register"
+    }
+  },
+  ForgetPassword: {
+    screen: ForgetPassword,
+    navigationOptions: {
+      title: "ForgetPassword"
+    }
+  },
+  Boiler: {
+    screen: Boiler,
+    navigationOptions: {
+      title: "Boiler"
+    }
+  }
+});
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
