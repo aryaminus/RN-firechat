e31840de76500c0b404d8cb3898262aee26bea58
Sunim Acharya <sunim.54@gmail.com>
2017-10-31 11:38:03 +0545

    Added Global Chat screen

---
+++ app/components/Backend.js
@@ -0,0 +1,48 @@
+import firebase from "react-native-firebase";
+class Backend {
+  uid = "";
+  messagesRef = null;
+  
+  setUid(value) {
+    this.uid = value;
+  }
+  getUid() {
+    return this.uid;
+  }
+  // retrieve the messages from the Backend
+  loadMessages(callback) {
+    this.messagesRef = firebase.database().ref("messages");
+    this.messagesRef.off();
+    const onReceive = data => {
+      const message = data.val();
+      callback({
+        _id: data.key,
+        text: message.text,
+        createdAt: new Date(message.createdAt),
+        user: {
+          _id: message.user._id,
+          name: message.user.name
+        }
+      });
+    };
+    this.messagesRef.limitToLast(20).on("child_added", onReceive);
+  }
+  // send the message to the Backend
+  sendMessage(message) {
+    for (let i = 0; i < message.length; i++) {
+      this.messagesRef.push({
+        text: message[i].text,
+        user: message[i].user,
+        createdAt: firebase.database.ServerValue.TIMESTAMP
+      });
+    }
+  }
+  // close the connection to the Backend
+  closeChat() {
+    if (this.messagesRef) {
+      this.messagesRef.off();
+    }
+  }
+}
+
+export default new Backend();
---
+++ app/components/GloChat.js
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react';
+import { Text, View, StyleSheet } from "react-native";
+import Backend from "../Backend";
+import { GiftedChat } from "react-native-gifted-chat";
+class Chat extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { name: false };
+  }
+  state = {
+    messages: []
+  };
+  
+  componentWillMount() {}
+
+  componentDidMount() {
+    Backend.loadMessages(message => {
+      this.setState(previousState => {
+        return {
+          messages: GiftedChat.append(previousState.messages, message)
+        };
+      });
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={message => {
+            Backend.sendMessage(message);
+          }}
+          user={{
+            _id: Backend.getUid(),
+            name: this.props.name
+          }}
+        />
+      </View>
+    );
+  }
+  componentWillUnmount() {
+    Backend.closeChat();
+  }
+}
+Chat.defaultProps = {
+  name: ""
+};
+Chat.prototypes = {
+  name: React.PropTypes.string
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "gray"
+  }
+});
+export default Chat;
