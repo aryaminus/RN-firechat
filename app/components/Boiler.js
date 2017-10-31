c8ed2926d1f3bc4abc680092e06ae28e517c7a56
Sunim Acharya <sunim.54@gmail.com>
2017-10-31 11:01:21 +0545

    Added Screens for Globalchat and friendlist

--- app/components/Boiler.js
+++ app/components/Boiler.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
 //import { StackNavigator } from "react-navigation";
 export default class Boiler extends Component {
+  state = {
+    name: ""
+  };
   static navigationOptions = {
     headerStyle: {
       backgroundColor: "#16a085",
@@ -10,10 +13,60 @@ export default class Boiler extends Component {
     headerLeft: null
   };
   render() {
-    return <Text>OLA</Text>;
+    return (
+      <View style={styles.containerl}>
+        <StatusBar barStyle="light-content" backgroundColor="#00796B" />
+        <Text style={styles.title}>Enter Your Name:</Text>
+        <TextInput
+          style={styles.nameInput}
+          placeholder={this.state.name}
+          onChangeText={text => {
+            this.setState({
+              name: text
+            });
+          }}
+          value={this.state.name}
+        />
+        <TouchableOpacity>
+          <Text
+            style={styles.buttonStyle}
+            onPress={() => this.props.navigation.navigate("GloChat", {name: this.state.name} )}
+          >
+            Chat Room
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Text
+          style={styles.buttonStyle}
+          onPress={() => this.props.navigation.navigate("Friendlist")}
+        >
+          Friend List
+        </Text>
+      </TouchableOpacity>
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+var styles = StyleSheet.create({
+  title: {
+    marginLeft: 20,
+    marginTop: 20,
+    fontSize: 20
+  },
+  nameInput: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 20
+  },
+  buttonStyle: {
+    marginLeft: 20,
+    margin: 20
+  },
+  containerl: {
+    flex: 1
+  }
+});
 
 AppRegistry.registerComponent("Boiler", () => Boiler);
