c8c29a8b4bc05755122bc20279b7d59f24eecb87
aryaminus <sunim.54@gmail.com>
2018-09-19 09:38:05 +0545

    Add firebase pod

--- ios/RNfirechat/AppDelegate.m
+++ ios/RNfirechat/AppDelegate.m
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -31,6 +31,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
